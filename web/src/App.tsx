51d59ebc39c55d98b25e322c6a912a34de42c3ab
guidsantos <guilherme.fast2503@gmail.com>
2020-11-14 23:26:11 -0300

    Pagina de Cadastro 1.0v

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 function App() {
-   return (
+  return (
     <>
-    <SignIn/>
-    <GlobalStyle/>
+      <SignUp />
+      <GlobalStyle />
     </>
-  )
+  );
 }
 
 export default App;
