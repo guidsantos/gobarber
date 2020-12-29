230f0f5fe41a3a7e573149406ee6f08e68a6e73e
guidsantos <guilherme.fast2503@gmail.com>
2020-12-29 06:45:24 -0300

    Tela de Cadastro

--- appgobarber/src/pages/SignUp/index.tsx
+++ appgobarber/src/pages/SignUp/index.tsx
@@ -1,12 +1,59 @@
 import React from 'react';
-import { Container } from './styles';
+import {
+  Image,
+  View,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-const SignUp: React.FC = () => {
+import { useNavigation } from '@react-navigation/native';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+
+const SignIn: React.FC = () => {
+  const navigation = useNavigation();
   return (
     <>
-      <Container />
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Crie Sua Conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button>Entrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+
+        <BackToSignInText>Volte para Logon</BackToSignInText>
+      </BackToSignIn>
     </>
   );
 };
 
-export default SignUp;
+export default SignIn;
